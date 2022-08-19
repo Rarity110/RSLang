fd90c2c88cb76a080e69f6c4ce46f9be443ac97a
ipipka <ya-pipka@yandex.ru>
2022-08-20 01:15:02 +0300

    feat: add component Footer

--- rslang/src/pages/HomePage.tsx
+++ rslang/src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import Header from '../components/Header/Header';
 import StartSection from '../components/home/StartSection/StartSection';
 import AboutApp from '../components/home/AboutApp/AboutApp';
 import AboutDevTeam from '../components/home/AboutDevTeam/AboutDevTeam';
+import Footer from '../components/Footer/Footer';
 
 const HomePage = () => {
   return (
@@ -12,7 +13,8 @@ const HomePage = () => {
       <StartSection />
       <AboutApp />
       <AboutDevTeam />
-      {/*<Footer />*/}
+
+      <Footer />
     </>
   );
 };
