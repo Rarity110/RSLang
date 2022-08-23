85cac819a73051c0787bb6d768f9ac1642018448
ipipka <ya-pipka@yandex.ru>
2022-08-24 02:17:01 +0300

    feat: fix style tag order

--- rslang/src/components/App/App.tsx
+++ rslang/src/components/App/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { StyledEngineProvider } from '@mui/material/styles';
 import AuthorizedPage from '../../pages/AuthorizedPage';
 import HomePage from '../../pages/HomePage';
 import StatisticsPage from '../../pages/StatisticsPage';
@@ -7,13 +8,15 @@ import classes from './App.module.scss';
 
 function App() {
   return (
-    <div className={classes.page}>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/statistics" element={<StatisticsPage />} />
-        <Route path="/auth-form" element={<AuthorizedPage />} />
-      </Routes>
-    </div>
+    <StyledEngineProvider injectFirst>
+      <div className={classes.page}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/statistics" element={<StatisticsPage />} />
+          <Route path="/auth-form" element={<AuthorizedPage />} />
+        </Routes>
+      </div>
+    </StyledEngineProvider>
   );
 }
 
