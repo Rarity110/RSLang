6b0411e85a121790ac64a36254f26134006ddd5e
Rarity110 <96205330+Rarity110@users.noreply.github.com>
2022-08-28 22:43:03 +0300

    Word list (#9)
    * refactor: refactor code
    
    * refactor: refactor code
    
    * feat: add sound to card
    
    * fix: fix sound at card

---
+++ rslang/src/components/Textbook/WordCard/AudioCard/AudioCard.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import VolumeDownSharpIcon from '@mui/icons-material/VolumeDownSharp';
+import classes from '../WordCard.module.scss';
+import { IAudio } from '../../consts';
+
+export class AudioCard extends Component<IAudio> {
+  audioList = [
+    new Audio(this.props.audio),
+    new Audio(this.props.audioMeaning),
+    new Audio(this.props.audioExample)
+  ];
+
+  render() {
+    return (
+      <div className={classes.sound}>
+        <VolumeDownSharpIcon onClick={() => this.props.func(this.audioList)} />
+      </div>
+    );
+  }
+}
