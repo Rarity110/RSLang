511ab1f68ddf127e8180f3949e8a1135d729d8a3
Rarity110 <rarity110@mail.ru>
2022-09-05 21:23:20 +0300

    refactor: change settings for deploy

--- rslang/src/index.tsx
+++ rslang/src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './components/App/App';
 import { AuthorizeState } from './components/auth-form/AuthorizeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <AuthorizeState>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </AuthorizeState>
 );
