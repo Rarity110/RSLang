9a30335b2a11ae4bdd43267608aaaf21e725199c
Rarity110 <96205330+Rarity110@users.noreply.github.com>
2022-09-06 00:44:29 +0300

    Word additional (#29)
    * feat: add information about games with the word
    
    * feat: add adaptive style
    
    * refactor: change settings for deploy
    
    * fix: fix information about games count
    
    * fix: fix information about games count
    
    * fix: fix http links
    
    * fix: fix logo

--- rslang/src/pages/TextbookPage.tsx
+++ rslang/src/pages/TextbookPage.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from '../components/Header/Header';
 import { App } from '../components/Textbook/App/App';
 import { Context } from '../components/App/Context';
+import Footer from '../components/Footer/Footer';
 
 export class TextbookPage extends Component {
   static contextType = Context;
@@ -12,6 +13,7 @@ export class TextbookPage extends Component {
       <>
         <Header {...{ isAuthorized }} />
         <App />
+        <Footer />
       </>
     );
   }
