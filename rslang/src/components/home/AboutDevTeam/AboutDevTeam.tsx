c953f7a462408ae0761b4fe45ea73eff09b15fe2
ipipka <ya-pipka@yandex.ru>
2022-09-06 03:19:21 +0300

    feat: main page - add data to section "about dev team"

--- rslang/src/components/home/AboutDevTeam/AboutDevTeam.tsx
+++ rslang/src/components/home/AboutDevTeam/AboutDevTeam.tsx
@@ -3,6 +3,8 @@ import { Box, Chip, Container, Divider, Typography } from '@mui/material';
 import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
 import Developer from '../Developer/Developer';
 import classes from './AboutDevTeam.module.scss';
+import avatarTatiana from '../../../assets/images/developer-tatiana.png';
+import avatarAlex from '../../../assets/images/developer-alex.png';
 import avatarDaria from '../../../assets/images/developer-daria.png';
 
 const AboutDevTeam = () => {
@@ -13,12 +15,11 @@ const AboutDevTeam = () => {
           Команда разработки
         </Typography>
 
-        {/* TODO: Дописать вклад каждого из участников */}
         <Developer
           name="Татьяна"
           githubName="rarity110"
-          description="Координировала рабочий процесс, распределяя задания и мотивируя команду. Взяла на себя вопросы, связанные с начальной настройкой проекта. Разработала дизайн и функционал электронного учебника и списка слов."
-          img=""
+          description="Координировала рабочий процесс, распределяя задания и мотивируя команду. Взяла на себя вопросы, связанные с начальной настройкой проекта. Разработала дизайн и функционал электронного учебника и списка слов. Принимала участие в разработке механизма изучения слов, настроила контекст для хранения слов для авторизованных пользователей"
+          img={avatarTatiana}
           rolesNode={
             <>
               <Chip label="Team lead" color="warning" icon={<AutoAwesomeIcon />} />
@@ -30,15 +31,15 @@ const AboutDevTeam = () => {
         <Developer
           name="Алексей"
           githubName="AVBr0"
-          description="Реализовал авторизацию пользователей в приложении, регистрацию новых пользователей"
-          img=""
+          description="Полностью взял на себя вопросы, связанные с регистрацией новых пользователей в системе и их дальнейшей авторизацией, настроил контекст для хранения информации о текущем пользователе. Реализовал игру «Спринт» и обработку ее результатов"
+          img={avatarAlex}
           rolesNode={<Chip label="Frontend developer" color="primary" />}
         />
         <Divider className={classes.aboutDevDivider} />
         <Developer
           name="Дарья"
           githubName="ipipka"
-          description="Разработала дизайн и функционал главной страницы."
+          description="Разработала дизайн и функционал главной страницы. Реализовала игру «Аудиовызов», страницу с ежедневной статистикой обучения. Принимала участие в разработке механизма изучения слов. Старалась не унывать :)"
           img={avatarDaria}
           rolesNode={<Chip label="Frontend developer" color="primary" />}
         />
